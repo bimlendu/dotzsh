9d364f70d32618824de585dee4798e08537f050b
Bimlendu Mishra <bimlendu@bluejeans.com>
2017-01-03 22:13:46 +0530

    adding aliases

---
+++ aliases.zsh
@@ -0,0 +1,21 @@
+# aliases
+
+alias mkdir="mkdir -p"
+alias cp="cp -r"
+alias scp="scp -r"
+
+alias vi="vim"
+
+alias ga="git add"
+alias gc="git commit -m"
+alias gs="git status"
+alias gd="git diff"
+alias gf="git fetch"
+alias gm="git merge"
+alias gr="git rebase"
+alias gp="git push"
+alias gu="git unstage"
+alias gg="git graph"
+alias gco="git checkout"
+
+alias tree='tree -CAFa -I "CVS|*.*.package|.svn|.git|.hg|node_modules|bower_components" --dirsfirst'
